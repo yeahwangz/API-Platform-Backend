5be983b6031079a2f2af8e8bc140988b9da7f09e
01geek <abc494823@gmail.com>
2024-12-01 00:44:36 +0800

    初始化项目

---
+++ src/main/java/com/geek01/apiplatformbackend/ApiPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.geek01.apiplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geek01/apiplatformbackend/ApiPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek01.apiplatformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
